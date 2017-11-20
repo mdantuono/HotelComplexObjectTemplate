465c4b55271608e299e973c9bb607abcc37ab953
Michael DAntuono <mikeishere3@gmail.com>
2017-11-20 17:47:57 +0000

    first commit, dynamic radio buttons

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,32 @@ var hotel = {
             available: 20
         },
         {
-            name: "Luxery Suite",
+            name: "Luxury Suite",
             price: "$250.00",
             available: 5
         }
     ],
     name: "CareerDevs Innt"
-}
+};
 
+// console.log(document.getElementById('label').nodeName);
+
+for (var i = 0; i < hotel.rooms.length; i++) {
+    console.log("start");
+    var radioBtn = document.createElement("INPUT");
+    var radioLabel = document.createElement("LABEL");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "rooms");
+    radioBtn.setAttribute("value", i);
+    radioBtn.setAttribute("id", "room" + i);
+    radioLabel.innerHTML = hotel.rooms[i].name;
+    console.log("stop");
+    
+    document.getElementById('radialSection').appendChild(radioBtn);
+    document.getElementById('radialSection').appendChild(radioLabel);
+}
+//display rooms as radio options
+
+//on form submission confirm radio was selected
+
+//and checkbox checked
